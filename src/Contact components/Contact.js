5c363a8be87588b4e0e698d0c16b9e0b589bcc4a
touron-travel <manivasagam.shanmugam@touron.in>
2020-10-24 12:26:34 +0530

    initial commit

--- src/Contact components/Contact.js
+++ src/Contact components/Contact.js
@@ -7,89 +7,78 @@ import Footer from "../Home components/Footer/Footer";
 export default function Contact() {
   return (
     <div className="contact">
-
       <div className="contact_top">
         <div className="nav">
-          <Navbar/>
+          <Navbar />
         </div>
       </div>
       <div className="contact_img">
         <div className="contact_title">
-             <p className="breadcrumbs"><span className="mr-2"><Link className="linkin" to="/">Home <i className="fa fa-chevron-right"></i></Link></span><span className="abt">Contact us <i className="fa fa-chevron-right"></i></span></p>
-             <h1 className="mb-0 bread">Contact Us</h1>
+          <p className="breadcrumbs">
+            <span className="mr-2">
+              <Link className="linkin" to="/">
+                Home <i className="fa fa-chevron-right"></i>
+              </Link>
+            </span>
+            <span className="abt">
+              Contact us <i className="fa fa-chevron-right"></i>
+            </span>
+          </p>
+          <h1 className="mb-0 bread">Contact Us</h1>
         </div>
-       </div>
+      </div>
 
-       <div className="address">
-         <div className="add">
-         <div className="icon">
-           <i className="fa fa-compass"></i>
-         </div>
-         <h3 className="connect">Address</h3>
-         <p className="locate">tour On Holidays,Workafella, Rathha Towers,
-              Tek Meadows - A Block,
-               4th Floor, Opposite to Accenture,
-               Sholinganallur, OMR,
-              Chennai-119</p>
-         </div>
-         <div className="add">
-         <div className="icon">
-           <i className="fa fa-phone"></i>
-         </div>
-         <h3 className="connect">Contact Number</h3>
-         <p className="locate">+91 86678 01206</p>
-         </div>
-         <div className="add">
-         <div className="icon">
-           <i className="fa fa-envelope"></i>
-         </div>
-         <h3 className="connect">Email Address</h3>
-         <p className="locate">hello@touron.in</p>
-         </div>
-       </div>
-
-       <div className="contact_information">
-        
-        <div className="contact_info">
-         <div className="contact_head">
-           <h1>Get in Touch</h1>
-         </div>
-        <div className="contact_form">
-         <div className="form-group">
-           <input type="text" className="form-control" placeholder="Your Name"/>
-         </div>
-         <div className="form-group">
-           <input type="text" className="form-control" placeholder="Mobile number"/>
-         </div>
-         <div className="form-group">
-           <input type="text" className="form-control" placeholder="Email address"/>
-         </div>
-         <div className="form-group">
-           <textarea name="" id="" cols="30" rows="5" className="form-control" placeholder="Message" />
+      <div className="address">
+        <div className="add">
+          <div className="icon">
+            <i className="fa fa-compass"></i>
+          </div>
+          <h3 className="connect">Address</h3>
+          <p className="locate">
+            tour On Holidays,Workafella, Rathha Towers, Tek Meadows - A Block,
+            4th Floor, Opposite to Accenture, Sholinganallur, OMR, Chennai-119
+          </p>
         </div>
-        <div>
-          <button type="submit" className="butto">Send Message</button>
-        </div>      
+        <div className="add">
+          <div className="icon">
+            <i className="fa fa-phone"></i>
+          </div>
+          <h3 className="connect">Contact Number</h3>
+          <p className="locate">+91 86678 01206</p>
         </div>
+        <div className="add">
+          <div className="icon">
+            <i className="fa fa-envelope"></i>
+          </div>
+          <h3 className="connect">Email Address</h3>
+          <p className="locate">hello@touron.in</p>
         </div>
+      </div>
 
-        <div className="contact_map">
-          <h3>Map</h3>
+      <div className="contact_info">
+        <div className="box">
+          <h2>Get in Touch</h2>
+          <form>
+            <div className="inputBox">
+              <input type="text" name="" required="name" />
+              <label>Name</label>
+            </div>
+            <div className="inputBox">
+              <input type="text" name="" required="number" />
+              <label>Mobile number</label>
+            </div>
+            <div className="inputBox">
+              <textarea required="message" />
+              <label>Message</label>
+            </div>
+            <input type="submit" className="bt" name="" value="Send Message" />
+          </form>
         </div>
+      </div>
 
-       </div>
-          
-      
-        
-
-         
-        <div className="footer">
-          <Footer />
-        </div>
-         
-
-       
-
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
