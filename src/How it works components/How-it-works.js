470b9b37e260dfe8995fd5efd56576a36afbcaec
touron-travel <manivasagam.shanmugam@touron.in>
2020-11-02 18:44:56 +0530

    first commit

--- src/How it works components/How-it-works.js
+++ src/How it works components/How-it-works.js
@@ -1,6 +1,48 @@
 import React from "react";
+import Navbar from "../Home components/Navbar/Navbar";
 import "./How-it-works.css";
+import { Link } from "react-router-dom";
+import Tourplan from "./Tourplan";
+import Footer from "../Home components/Footer/Footer";
 
 export default function HowItWorks() {
-  return <div>how it works</div>;
+  return (
+    <div className="work">
+      <div className="work_top">
+        <div className="nav">
+          <Navbar />
+        </div>
+      </div>
+      <div className="work_img">
+        <div className="work_title">
+          <p className="breadcrumbs">
+            <span className="mr-2">
+              <Link className="linkin" to="/">
+                Home <i className="fa fa-chevron-right"></i>
+              </Link>
+            </span>
+            <span className="abt">
+              How it Works <i className="fa fa-chevron-right"></i>
+            </span>
+          </p>
+          <h1 className="mb-0 bread">How it works</h1>
+        </div>
+      </div>
+      <div className="work_overview">
+        <h4>Overview</h4>
+        <p>
+          TourOn provides different types of tour packages which include Luxury,
+          Honeymoon Tour, Planned Tour, Surprise Tour or a specially organised
+          Road Trip. Destinations will have public transportation and plenty of
+          resources and activities to keep you busy.
+        </p>
+      </div>
+      <div>
+        <Tourplan />
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
+  );
 }
---
+++ src/How it works components/Tourplan.js
@@ -0,0 +1,122 @@
+import React from "react";
+import "./Tourplan.css";
+import { Link } from "react-scroll";
+import cat1 from "../assests/cat1.jpg";
+import cat2 from "../assests/cat2.jpg";
+import cat3 from "../assests/cat3.jpg";
+import cat4 from "../assests/cat4.jpg";
+import cat5 from "../assests/cat5.jpg";
+
+export default function tourplan() {
+  return (
+    <>
+      <div className="work_categories">
+        <div className="work_cat-body">
+          <img className="work_cat-image" src={cat4} alt="" />
+          <div className="work_cat-title">
+            <Link
+              activeClass="active"
+              to="work_luxurytour"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <p>Luxury</p>
+            </Link>
+            {/* <a href="#work_luxurytour">
+              <p>Luxury</p>
+            </a> */}
+          </div>
+        </div>
+        <div className="work_cat-body">
+          <img className="work_cat-image" src={cat5} alt="" />
+          <div className="work_cat-title">
+            <Link
+              activeClass="active"
+              to="work_honeytour"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <p>Honeymoon Tour</p>
+            </Link>
+            {/* <a href="#work_honeytour">
+              <p>Honeymoon Tour</p>
+            </a> */}
+          </div>
+        </div>
+        <div className="work_cat-body">
+          <img className="work_cat-image" src={cat1} alt="" />
+          <div className="work_cat-title">
+            <Link
+              activeClass="active"
+              to="work_plannedtour"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <p>Planned Tour</p>
+            </Link>
+            {/* <a href="#work_plannedtour">
+              <p>Planned Tour</p>
+            </a> */}
+          </div>
+        </div>
+        <div className="work_cat-body">
+          <img className="work_cat-image" src={cat2} alt="" />
+          <div className="work_cat-title">
+            <Link
+              activeClass="active"
+              to="work_surprisetour"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <p>Surprise Tour</p>
+            </Link>
+            {/* <a href="#work_surprisetour">
+              <p>Surprise Tour</p>
+            </a> */}
+          </div>
+        </div>
+        <div className="work_cat-bod">
+          <img className="work_cat-image" src={cat3} alt="" />
+          <div className="work_cat-title">
+            <Link
+              activeClass="active"
+              to="work_roadtour"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <p>Road Trip</p>
+            </Link>
+            {/* <a href="#work_roadtour">
+              <p>Road Trip</p>
+            </a> */}
+          </div>
+        </div>
+      </div>
+      <div id="work_luxurytour">
+        <h3>Luxury</h3>
+      </div>
+      <div id="work_honeytour">
+        <h3>Honeymoon Tour</h3>
+      </div>
+      <div id="work_plannedtour">
+        <h3>Planned Tour</h3>
+      </div>
+      <div id="work_surprisetour">
+        <h3>Surprise Tour</h3>
+      </div>
+      <div id="work_roadtour">
+        <h3>Road Trip</h3>
+      </div>
+    </>
+  );
+}
