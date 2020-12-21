f4c08744c8744de72376aaee7136950a9087b2f2
Dk2626 <52568067+Dk2626@users.noreply.github.com>
2020-12-21 12:38:41 +0530

    working about page responsive

--- src/About components/About.js
+++ src/About components/About.js
@@ -43,33 +43,26 @@ export default function About() {
         </div>
       </div>
       <div className="intro">
-        <div className="row">
-          <div className="col-lg-7">
-            <img className="intro_image" src={intro} alt="" />
-          </div>
-          <div className="col-lg-5">
-            <div className="intro_content">
-              <div className="intro_title">we have the best tours</div>
-              <p className="intro_text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-                quis vulputate eros, iaculis consequat nisl. Nunc et suscipit
-                urna. Integer elementum orci eu vehicula pretium. Donec bibendum
-                tristique condimentum. Aenean in lacus ligula. Phasellus euismod
-                gravida eros. Aenean nec ipsum aliquet, pharetra magna id,
-                interdum sapien. Etiam id lorem eu nisl pellentesque semper.
-                Nullam tincidunt metus placerat, suscipit leo ut, tempus nulla.
-                Fusce at eleifend tellus. Ut eleifend dui nunc, non fermentum
-                quam placerat non. Etiam venenatis nibh augue, sed eleifend
-                justo tristique eu
-              </p>
-              <div className="btns">
-                <a href="#">
-                  explore now<span></span>
-                  <span></span>
-                  <span></span>
-                </a>
-              </div>
-            </div>
+        <img className="intro_image" src={intro} alt="" />
+        <div className="intro_content">
+          <div className="intro_title">we have the best tours</div>
+          <p className="intro_text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            quis vulputate eros, iaculis consequat nisl. Nunc et suscipit urna.
+            Integer elementum orci eu vehicula pretium. Donec bibendum tristique
+            condimentum. Aenean in lacus ligula. Phasellus euismod gravida eros.
+            Aenean nec ipsum aliquet, pharetra magna id, interdum sapien. Etiam
+            id lorem eu nisl pellentesque semper. Nullam tincidunt metus
+            placerat, suscipit leo ut, tempus nulla. Fusce at eleifend tellus.
+            Ut eleifend dui nunc, non fermentum quam placerat non. Etiam
+            venenatis nibh augue, sed eleifend justo tristique eu
+          </p>
+          <div className="btns">
+            <a href="#">
+              explore now<span></span>
+              <span></span>
+              <span></span>
+            </a>
           </div>
         </div>
       </div>
