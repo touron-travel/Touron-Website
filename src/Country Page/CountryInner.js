0ba8e3b75f1800409d097aea7ae843e90f96c77e
Mani1211 <manivasagam.shanmugam@touron.in>
2020-12-22 13:18:19 +0530

    Country Inner worked

--- src/Country Page/CountryInner.js
+++ src/Country Page/CountryInner.js
@@ -85,6 +85,60 @@ const CountryInner = () => {
     arrows: true,
     slidesToShow: 6.5,
     slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 360,
+        settings: {
+          slidesToShow: 1.5,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 280,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+    ],
   };
   return (
     <div className="countryInner_Details">
@@ -235,7 +289,7 @@ const CountryInner = () => {
                 </div>
               </div>
             </div>
-            <div className="countryInner_aboutRight">Content</div>
+            {/* <div className="countryInner_aboutRight">Content</div> */}
           </div>
           <div className="highlights">
             <div className="highlights_title">
